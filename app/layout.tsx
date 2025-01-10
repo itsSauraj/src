5b983af9fb44be54f347eafe00f895243fad7908
Saurabh Yadav <sauraj.contact@gmail.com>
2025-01-10 18:52:41 +0530

    add utility functions, update Tailwind configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,12 +39,8 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
-          <div className="relative flex flex-col h-screen">
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              {children}
-            </main>
-          </div>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          {children}
         </Providers>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import { siteConfig } from "@/config/site";
-import { title } from "@/components/primitives";
+import { SideBar } from "@/components/dashboard";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <h1 className={title({ color: "violet", size: "lg" })}>
-        {siteConfig.name}
-      </h1>
-    </section>
+    <>
+      <SideBar />
+    </>
   );
 }
